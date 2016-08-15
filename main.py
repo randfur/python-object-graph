0207fbc3532637347f4a1d152f5d39622a65650d
Alan Cutter <alancutter@chromium.org>
2016-08-15 13:02:12 +1000

    First commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+
+# Recursively following the attributes of these types results in infinite recursion.
+dontFollowTypes = set([
+  type(None.__init__),
+  type(None.__sizeof__),
+])
+
+def getAttribute(obj, attribute):
+  try:
+    value = None.__class__.__getattribute__(obj, attribute)
+  except AttributeError:
+    if obj == type and attribute == '__abstractmethods__':
+      return None, True
+    raise
+  return value, False
+
+def doubleQuote(string):
+  return '"%s"' % string.replace('"', '\\"')
+
+def objectName(obj):
+  name = repr(obj)
+  if type(obj) in dontFollowTypes:
+    # Cut out the memory address part:
+    # This "<method-wrapper '__init__' of NoneType object at 0x91a870>"
+    # becomes this "<method-wrapper '__init__' of NoneType object>"
+    name = name[:-13] + name[-1:]
+  return name
+
+def graphObjectAttributes(rootObject):
+  unprocessed = [rootObject]
+  nameToId = {}
+  idToName = {}
+  def getId(obj):
+    name = objectName(obj)
+    if name not in nameToId:
+      id = len(nameToId)
+      nameToId[name] = id
+      idToName[id] = name
+    return nameToId[name]
+  graph = {}
+  while unprocessed:
+    obj = unprocessed.pop()
+    edges = {}
+    graph[getId(obj)] = edges
+    for attribute in dir(obj):
+      nextObj, err = getAttribute(obj, attribute)
+      if err:
+        continue
+      nextId = getId(nextObj)
+      edges[attribute] = nextId
+      if type(nextObj) not in dontFollowTypes and nextId not in graph:
+        unprocessed.append(nextObj)
+  return graph, idToName
+
+def printGraph(graph, idToName):
+  print('digraph {')
+  for id in graph:
+    name = idToName[id]
+    print('  %s [label=%s];' % (id, doubleQuote(name)))
+    for attribute, nextId in graph[id].items():
+      print('  %s -> %s [label=%s];' % (id, nextId, doubleQuote(attribute)))
+    print()
+  print('}')
+
+graph, idToName = graphObjectAttributes(None)
+printGraph(graph, idToName)
